3d87b7157890d49edc8c258dc3c9abe4537ae17d
veera <veerabrahmam416@gmail.com>
2023-06-15 14:43:56 +0530

    added project files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This line is added by Veera from Github.com</h1>
+      <h1>This second line is added by Veera from local repository</h1>
+      
+    </div>
+  );
+}
+
+export default App;
